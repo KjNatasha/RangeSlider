ba4337f2278d2320db41912d25a28853ca96131b
Kyeongjin Jeong <jeongdongjin@jeongdongjin-ui-MacBook-Pro-2.local>
2017-05-04 15:45:31 +0900

    trackLayer frame changed(3/8) (RangeSlider.swift, line 229)

--- RangeSlider/RangeSlider.swift
+++ RangeSlider/RangeSlider.swift
@@ -226,7 +226,7 @@ public class RangeSlider: UIControl {
         CATransaction.begin()
         CATransaction.setDisableActions(true)
         
-        trackLayer.frame = bounds.insetBy(dx: 0.0, dy: bounds.height/4)
+        trackLayer.frame = bounds.insetBy(dx: 0.0, dy: bounds.height/(8/3))
         trackLayer.setNeedsDisplay()
         
         let lowerThumbCenter = CGFloat(positionForValue(lowerValue))
